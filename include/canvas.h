64ce00a6c2317bc2f320afe77b45b051696e3208
Christoph <christoph.bodner@gmail.com>
2021-06-06 18:57:34 +0200

    first prototype with working canvas drawing

---
+++ include/canvas.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CImg.h"
+#include <array>
+
+using namespace cimg_library;
+
+
+class Canvas {
+    public:
+        Canvas(int x, int y);
+        void put_pixel(int x, int y, std::array<int, 3> rgb);
+        void display();
+    private:
+        int _x;
+        int _y;
+        CImg<unsigned int> _img;
+        std::array<int, 3> clamp_colors(std::array<int, 3> rgb);
+};
+
+// TODO: Can I remove std::array and still use {} in function call somehow? https://stackoverflow.com/questions/29360555/c-passing-an-array-directly-into-a-function-without-initializing-it-first
---
+++ src/canvas.cpp
@@ -0,0 +1,50 @@
+#include "canvas.h"
+#include "CImg.h"
+
+using namespace cimg_library;
+
+Canvas::Canvas(int x, int y) : _x(x), _y(y)
+{
+    //initialize image of dimension width, height with white bg
+    _img = CImg<unsigned int>(x, y, 1, 3, 255);
+}
+
+std::array<int, 3> Canvas::clamp_colors(std::array<int, 3> rgb)
+{
+
+    //Make sure colors are inside range
+    for (int i = 0; i <= 2; i++)
+    {
+        if (rgb[i] < 0)
+        {
+            rgb[i] = 0;
+        }
+        else if (rgb[i] > 255)
+        {
+            rgb[i] = 255;
+        }
+    }
+
+    return rgb;
+}
+
+void Canvas::put_pixel(int x, int y, std::array<int, 3> rgb)
+{
+
+    auto rgb_clamped = clamp_colors(rgb);
+
+    //Put pixel relative to center of canvas
+    int s_x = this->_x/2 + x;
+    int s_y = this->_y/2 - y;
+
+    for (int i = 0; i <= 2; i++)
+    {
+        _img(s_x, s_y, 0, i) = rgb_clamped[i];
+    }
+}
+
+void Canvas::display()
+{
+    _img.display("Simple Raytracer");
+}
+
